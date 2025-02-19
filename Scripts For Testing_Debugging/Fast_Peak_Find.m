f6dc6dce0193644b4dd2ef92df5de616150eb59b
Bera Yavuz <realberayavuz@gmail.com>
2025-02-19 18:03:27 -0500

    random walk algorithm

--- Scripts For Testing_Debugging/Fast_Peak_Find.m
+++ Scripts For Testing_Debugging/Fast_Peak_Find.m
@@ -1,68 +1,77 @@
-% Bera Yavuz
-% Object with all required functions
-MyFuncs = functionsContainer;
-MyFuncs.AddPathFunc("LAB"); 
-% Establishing serialconnetion with ANC300 device
-%ANC300 = serialport("COM7",9600); % Change X to the COM port connected
+% random walker algorithm 
 
-check_x_move_step = 4;
-check_y_move_step = 4; 
-Frequency = 20; 
-
-            % Init
-            Photon_count_init = py.ID900_Func.init_ID900();
+% Random walker parameters
+num_steps = 5000; % Number of steps for the random walk
+start_position = [0, 0]; % Start from an arbitrary point away from the center
+positions = start_position; % Initialize positions array
 
 
-            % Get the original photon count
-            original_count = py.ID900_Func.query_photon_counter(Photon_count_init); 
-            
-            % Movement commands (forward and backward) - using fprintf with anc400
-            move_x_back_cmd = sprintf("stepd 1 %d", check_x_move_step); move_x_forward_cmd = sprintf("stepu 1 %d", check_x_move_step);
-            move_y_back_cmd = sprintf("stepd 2 %d", check_y_move_step); move_y_forward_cmd = sprintf("stepu 2 %d", check_y_move_step);
+% Function for stochastic movement
+function [x_pos_new, y_pos_new] = new_stochastic_movement(x_pos, y_pos, dir_x, dir_y,ANC300,Frequency)
+    
+    % Calculate the random perturbation (random step size)
+    step_size_x = randi([3 6],1); % Random step size between 3 and 6
+    step_size_y = randi([3 6],1); % Random step size between 3 and 6
 
-            move_x = {@() fprintf(ANC300, move_x_back_cmd), ...
-                      @() fprintf(ANC300, move_x_forward_cmd)}; % X+ (right), X- (left)
-            move_y = {@() fprintf(ANC300, move_y_back_cmd), ...
-                      @() fprintf(ANC300, move_y_forward_cmd)}; % Y+ (up), Y- (down)
-            
-            % Directions: {Move forward, Move backward}
-            directions = {move_x{1}, move_x{2}, move_y{1}, move_y{2}};
-            reverse_directions = {move_x{2}, move_x{1}, move_y{2}, move_y{1}}; % Reverse each direction
-            step_types = [check_x_move_step check_y_move_step]; 
-            
-            % main while loop that iterates each 4 direction movement
-            no_movement = 0; 
-            while no_movement ~= 4
-            
-            % counter to see how many times a movement didn't happen for each direct
-            no_movement = 0; 
-            
-                % for loop used for going in each direction  
-                for check = 1:4
-                
-                    while true
-                        % Move in the specified direction
-                        directions{check}();
-                        if check == 1 || check == 2
-                            step_num = step_types(1);
-                        else
-                            step_num = step_types(2);
-                        end
-                        MyFuncs.StepQueue(step_num,Frequency);
-                
-                        
-                        % Get the new photon count
-                        new_count = py.ID900_Func.query_photon_counter(Photon_count_init); 
-                        pause(1)
-                        % If photon count increases, update original count and continue moving
-                        if new_count > original_count
-                            original_count = new_count; 
-                        else
-                            % Move back to the previous position and break out of loop
-                            reverse_directions{check}();
-                            no_movement = no_movement+1; 
-                            break;
-                        end
-                    end
-                end
-            end
+
+    % step direction command 
+    if dir_x == 1
+    stochastic_dir_x = sprintf("stepu 1 %d",step_size_x);
+    x_factor = 1; 
+    else
+    stochastic_dir_x = sprintf("stepd 1 %d",step_size_x);
+    x_factor = -1; 
+    end
+    
+    if dir_y == 1
+    stochastic_dir_y = sprintf("stepu 2 %d",step_size_y);
+    y_factor = 1;
+    else
+    stochastic_dir_y = sprintf("stepd 2 %d",step_size_y);
+    y_factor = -1; 
+    end
+
+    
+
+    % Update position
+    x_pos_new = x_pos + x_factor*step_size_x;
+    y_pos_new = y_pos + y_factor*step_size_y;
+
+    % applying actual movement 
+    fprintf(ANC300,stochastic_dir_x)
+    Stepqueue(obj,step_size_x,Frequency);
+
+    fprintf(ANC300,stochastic_dir_y)
+    Stepqueue(obj,step_size_y,Frequency);
+    
+    
+end
+
+% Simulate the biased random walk
+for step = 1:num_steps
+    % Get current position
+    current_pos = positions(end, :);
+    x_pos = current_pos(1);
+    y_pos = current_pos(2);
+
+    % Randomly choose direction for x and y
+    dir_x = 2*randi([0,1])-1; 
+    dir_y = 2*randi([0,1])-1; 
+
+    % Calculate new position
+    [x_pos_new, y_pos_new] = new_stochastic_movement(x_pos, y_pos, dir_x, dir_y);
+
+    % Get the change in photon count 
+    photon_counts = py.ID900_Func.query_photon_counter(Photon_count_init);
+
+    % If intensity decreases, retrace step
+    if photon_counts < 0
+        [x_pos_new, y_pos_new] = new_stochastic_movement(x_pos_new, y_pos_new, -dir_x, -dir_x);
+    end
+
+    % If intensity increases or stays the same, accept the new position
+    if photon_counts >= 0
+        positions = [positions; x_pos_new, y_pos_new]; % Append new position
+    end
+end
+
